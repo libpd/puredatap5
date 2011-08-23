e668d004e9689512af161b7f2158b323f701084e
Peter Brinkmann <peter.brinkmann@gmail.com>
2011-08-22 22:36:20 -0400

    updated for latest version of libpd

---
+++ src/com/noisepages/nettoyeur/processing/PureDataP5Jack.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ * For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ * WARRANTIES, see the file, "LICENSE.txt," in this distribution.
+ * 
+ */
+
+package com.noisepages.nettoyeur.processing;
+
+
+import org.puredata.jack.PdJackProcessor;
+import org.puredata.processing.PureDataP5Base;
+
+import processing.core.PApplet;
+
+import com.noisepages.nettoyeur.jack.JackException;
+import com.noisepages.nettoyeur.jack.JackNativeClient;
+
+
+/**
+ * Support for using Pd and Jack with Processing.
+ * 
+ * @author Peter Brinkmann (peter.brinkmann@gmail.com)
+ */
+public class PureDataP5Jack extends PureDataP5Base {
+
+	private JackNativeClient client = null;
+	private final PdJackProcessor processor;
+	private final String source, sink;
+
+	public PureDataP5Jack(PApplet parent, int nIn, int nOut) {
+		this(parent, nIn, nOut, null, null);
+	}
+	
+	public PureDataP5Jack(PApplet parent, int nIn, int nOut, String source, String sink) {
+		super(parent);
+		this.source = source;
+		this.sink = sink;
+		parent.registerDispose(this);
+		try {
+			processor = PdJackProcessor.createPdJackProcessor(nIn, nOut);
+		} catch (JackException e) {
+			// Converting to an unchecked exception is questionable from a Java point of view,
+			// but it's the Processing way.
+			throw new RuntimeException(e);
+		}
+	}
+	
+	@Override
+	public void start()  {
+		stop();
+		try {
+			client = processor.createClient("processing");
+			if (source != null) {
+				client.connectInputPorts(source);
+			}
+			if (sink != null) {
+				client.connectOutputPorts(sink);
+			}
+		} catch (JackException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void stop() {
+		if (client != null) {
+			client.close();
+			client = null;
+		}
+	}
+}
