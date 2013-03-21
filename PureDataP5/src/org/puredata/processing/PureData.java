cf4c2e6bf9df0c2efbf95c846e5201abf2a9d81e
Peter Brinkmann <peter.brinkmann@gmail.com>
2013-03-21 14:59:35 -0400

    updated for latest version of libpd, plus MIDI support

--- PureDataP5/src/org/puredata/processing/PureData.java
+++ PureDataP5/src/org/puredata/processing/PureData.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.puredata.core.PdBase;
+import org.puredata.core.PdMidiReceiver;
 import org.puredata.core.PdReceiver;
 
 import processing.core.PApplet;
@@ -24,15 +25,23 @@ import processing.core.PApplet;
  * 
  * @author Peter Brinkmann (peter.brinkmann@gmail.com)
  */
-public class PureData implements PdReceiver {
+public class PureData implements PdReceiver, PdMidiReceiver {
 
 	private final PApplet parent;
+	
 	private final Method pdPrintCallback;
 	private final Method receiveBangCallback;
 	private final Method receiveFloatCallback;
 	private final Method receiveSymbolCallback;
 	private final Method receiveListCallback;
 	private final Method receiveMessageCallback;
+	
+	private final Method receiveNoteOnCallback;
+	private final Method receivePolyAftertouchCallback;
+	private final Method receiveControlChangeCallback;
+	private final Method receiveProgramChangeCallback;
+	private final Method receiveAftertouchCallback;
+	private final Method receivePitchBendCallback;
 
 	/**
 	 * @param parent             owner
@@ -42,8 +51,8 @@ public class PureData implements PdReceiver {
 	 */
 	public PureData(PApplet parent, int sampleRate, int inputChannels, int outputChannels) {
 		this.parent = parent;
-		parent.registerDispose(this);
-		parent.registerPre(this);
+		parent.registerMethod("dispose", this);
+		parent.registerMethod("pre", this);
 		if (!PdBase.implementsAudio()) {
 			throw new RuntimeException("PdBase does not implement audio!");
 		}
@@ -51,6 +60,7 @@ public class PureData implements PdReceiver {
 			throw new RuntimeException("Bad audio parameters: " + inputChannels + ", " + outputChannels + ", " + sampleRate);
 		}
 		PdBase.setReceiver(this);
+		PdBase.setMidiReceiver(this);
 		PdBase.computeAudio(true);
 		PdBase.clearSearchPath();
 		PdBase.addToSearchPath(parent.dataPath(""));
@@ -61,6 +71,12 @@ public class PureData implements PdReceiver {
 		receiveSymbolCallback = methods.get("receiveSymbol");
 		receiveListCallback = methods.get("receiveList");
 		receiveMessageCallback = methods.get("receiveMessage");
+		receiveNoteOnCallback = methods.get("receiveNoteOn");
+		receivePolyAftertouchCallback = methods.get("receivePolyAftertouch");
+		receiveControlChangeCallback = methods.get("receiveControlChange");
+		receiveProgramChangeCallback = methods.get("receiveProgramChange");
+		receiveAftertouchCallback = methods.get("receiveAftertouch");
+		receivePitchBendCallback = methods.get("receivePitchBend");
 	}
 	
 	private Map<String, Method> extractMethods(Class<?> clazz) {
@@ -84,7 +100,8 @@ public class PureData implements PdReceiver {
 	 * Processing pre-draw callback, automatically registered in the constructor.
 	 */
 	public void pre() {
-		PdBase.pollMessageQueue();
+		PdBase.pollMidiQueue();
+		PdBase.pollPdMessageQueue();
 	}
 	
 	/**
@@ -190,6 +207,48 @@ public class PureData implements PdReceiver {
 	}
 	
 	/**
+	 * Delegates to the corresponding method in {@link PdBase}.
+	 */
+	public void sendNoteOn(int channel, int note, int velocity) {
+		PdBase.sendNoteOn(channel, note, velocity);
+	}
+	
+	/**
+	 * Delegates to the corresponding method in {@link PdBase}.
+	 */
+	public void sendPolyAftertouch(int channel, int note, int velocity) {
+		PdBase.sendPolyAftertouch(channel, note, velocity);
+	}
+	
+	/**
+	 * Delegates to the corresponding method in {@link PdBase}.
+	 */
+	public void sendControlChange(int channel, int controller, int value) {
+		PdBase.sendControlChange(channel, controller, value);
+	}
+	
+	/**
+	 * Delegates to the corresponding method in {@link PdBase}.
+	 */
+	public void sendAftertouch(int channel, int value) {
+		PdBase.sendAftertouch(channel, value);
+	}
+	
+	/**
+	 * Delegates to the corresponding method in {@link PdBase}.
+	 */
+	public void sendPitchBend(int channel, int value) {
+		PdBase.sendPitchBend(channel, value);
+	}
+	
+	/**
+	 * Delegates to the corresponding method in {@link PdBase}.
+	 */
+	public void sendProgramChange(int channel, int value) {
+		PdBase.sendProgramChange(channel, value);
+	}
+	
+	/**
 	 * Delegates to parent's pdPrint method, if it exists; no-op otherwise.
 	 */
 	@Override
@@ -271,5 +330,86 @@ public class PureData implements PdReceiver {
 				// Do nothing.
 			}
 		}
+	}
+
+	@Override
+	public boolean beginBlock() {
+		return false;
+	}
+
+	@Override
+	public void endBlock() {
+		// Do nothing.
+	}
+
+	@Override
+	public void receiveAftertouch(int c, int v) {
+		if (receiveAftertouchCallback != null) {
+			try {
+				receiveAftertouchCallback.invoke(parent, c, v);
+			} catch (Exception e) {
+				// Do nothing.
+			}
+		}
+	}
+
+	@Override
+	public void receiveControlChange(int c, int k, int v) {
+		if (receiveControlChangeCallback != null) {
+			try {
+				receiveControlChangeCallback.invoke(parent, c, k , v);
+			} catch (Exception e) {
+				// Do nothing.
+			}
+		}
+	}
+
+	@Override
+	public void receiveNoteOn(int c, int k, int v) {
+		if (receiveNoteOnCallback != null) {
+			try {
+				receiveNoteOnCallback.invoke(parent, c, k, v);
+			} catch (Exception e) {
+				// Do nothing.
+			}
+		}
+	}
+
+	@Override
+	public void receivePitchBend(int c, int v) {
+		if (receivePitchBendCallback != null) {
+			try {
+				receivePitchBendCallback.invoke(parent, c, v);
+			} catch (Exception e) {
+				// Do nothing.
+			}
+		}
+	}
+
+	@Override
+	public void receivePolyAftertouch(int c, int k, int v) {
+		if (receivePolyAftertouchCallback != null) {
+			try {
+				receivePolyAftertouchCallback.invoke(parent, c, k, v);
+			} catch (Exception e) {
+				// Do nothing.
+			}
+		}
+	}
+
+	@Override
+	public void receiveProgramChange(int c, int v) {
+		if (receiveProgramChangeCallback != null) {
+			try {
+				receiveProgramChangeCallback.invoke(parent, c, v);
+			} catch (Exception e) {
+				// Do nothing.
+			}
+		}
+	}
+
+	@Override
+	public void receiveMidiByte(int p, int b) {
+		// Do nothing; we don't support this low-level functionality in Processing.
 	}
 }
